d9c58acb30e03c5c1ca9e5a4cbc5979454f54fb4
Obi Fortune <gabrielobi.of@gmail.com>
2021-04-03 14:18:18 +0300

    url fixes(was not directing me to the page)

--- backend/server.js
+++ backend/server.js
@@ -39,19 +39,34 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/config", configRoutes);
 
-// const __dirname = path.resolve();
+// // const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//     res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API IS RUNNING...");
+//   });
+// }
+
+const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-    res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
-    res.send("API IS RUNNING...");
+    res.send("API is running....");
   });
 }
 
